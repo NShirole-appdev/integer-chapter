abaf07a064dfbb39d0fc0f2992255ec1db315f35
Nina Shirole <ninashirole@uchicago.edu>
2021-07-06 02:13:34 +0000

    integer_birth_year attempt

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -7,3 +7,7 @@
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+age = gets.to_i
+current_year = Time.new.year
+year = current_year - age
+p "Wow, you were born in #{year}. You’re old!"
